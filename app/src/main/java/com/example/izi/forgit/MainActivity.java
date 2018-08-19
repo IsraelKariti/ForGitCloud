fbfb8b5f7770dd4e281d656038527e3ccf71fda7
aa <mrkarity@gmail.com>
2018-08-19 21:20:00 +0300

    START 8

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int MASTER = 8;
+        int MASTER = 9;
     }
 }
