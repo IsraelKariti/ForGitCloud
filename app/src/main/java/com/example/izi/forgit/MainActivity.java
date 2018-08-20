9ee97d05bcc6cabd46d43d6c83e88c434e7c972d
IsraelKariti <mrkarity@gmail.com>
2018-08-20 13:28:41 +0300

    add on cloud onCloud integer

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int MASTER_server = 102;
+        int onCloud = 88;
     }
 }
