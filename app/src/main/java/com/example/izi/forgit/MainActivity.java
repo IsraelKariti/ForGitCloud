7e69c1771f903acb1d13cd4639cb80d4b75b7ab3
aa <mrkarity@gmail.com>
2018-08-20 17:41:59 +0300

    big 1

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int START = 0;
+        int big = 1;
     }
 }
