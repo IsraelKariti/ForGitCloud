378074f9fa2addbdaaea34569d8df586e71bfd53
aa <mrkarity@gmail.com>
2018-08-20 17:09:26 +0300

    coconut 2

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         int change_merge = 3;
         int change_branch = 1;
         int monkey = 3;
-        int coconut = 1;
+        int coconut = 2;
     }
 }
