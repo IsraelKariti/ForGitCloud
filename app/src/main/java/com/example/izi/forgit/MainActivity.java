863b5aef42e3c58a334d515d90d5ab587044d3c3
aa <mrkarity@gmail.com>
2018-08-20 13:16:41 +0300

    START 102

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int MASTER_server = 101;
+        int MASTER_server = 102;
     }
 }
