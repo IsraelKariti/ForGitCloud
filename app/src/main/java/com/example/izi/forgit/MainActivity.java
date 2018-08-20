982df1837a90752bae19209d9504de2f7e29605b
aa <mrkarity@gmail.com>
2018-08-20 17:21:23 +0300

    REAL BASE

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -9,17 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int MASTER_server = 102;
-        int onCloud = 88;
-        int onLocal = 77;
-        int change_local_version_of_branch_cloud = 0;
-        int change_local = 3;
-        int change_merge = 3;
-        int change_branch = 1;
-        int monkey = 4;
-        int coconut = 4;
-        int base = 1;
-        int major = 2;
-        int minor = 3;
+
     }
 }
