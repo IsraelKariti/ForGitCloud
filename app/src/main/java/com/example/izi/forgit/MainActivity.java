c66721e4b390f7dd82363bd7b5ee47f211d494c8
aa <mrkarity@gmail.com>
2018-08-21 11:21:24 +0300

    new_monkey 1

--- app/src/main/java/com/example/izi/forgit/MainActivity.java
+++ app/src/main/java/com/example/izi/forgit/MainActivity.java
@@ -9,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int MASTER_server = 102;
-        int onCloud = 88;
-        int onLocal = 77;
-        int change_local_version_of_branch_cloud = 0;
-        int change_local = 3;
-        int change_merge = 3;
-        int change_branch = 1;
-        int monkey = 2;
+        int new_monkey = 1;
     }
 }
